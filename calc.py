4825184ca1c65debd0ab339d8f1ff7d5dc88a0ff
anvesh <anvesh1013@gmail.com>
2019-12-10 05:29:11 +0000

    Modified calc function

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """subtract Function"""
+    return x - y
+
+def multiply(x, y):
+    """multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """divide Function"""
+    return x / y
